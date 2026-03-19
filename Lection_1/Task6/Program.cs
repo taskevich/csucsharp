d370faa18d528ecc2baedd30e815f2d1e5dfb3ce
odnoburtsev_vadim <taskevichoff@yandex.ru>
2026-03-19 11:48:04 +0500

    solve tasks

--- Lection_1/Task6/Program.cs
+++ Lection_1/Task6/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿Point a = new Point(10, 20);
+Point b = new Point(30, 40);
+Point c = new Point(50, 30);
+
+double resultDistance = DistanceToLine(a, b, c);
+Console.WriteLine("Дистанция = {0}", resultDistance);
+
+double DistanceToLine(Point a, Point b, Point c)
+{
+    double abX = b.x - a.x;
+    double abY = b.y - a.y;
+
+    double apX = c.x - a.x;
+    double apY = c.y - a.y;
+
+    double crossProduct = Math.Abs(abX * apY - abY * apX);
+
+    double lengthAB = Math.Sqrt(abX * abX + abY * abY);
+
+    if (lengthAB < double.Epsilon)
+    {
+        double dx = c.x - a.x;
+        double dy = c.y - a.y;
+        return Math.Sqrt(dx * dx + dy * dy);
+    }
+
+    double distance = crossProduct / lengthAB;
+
+    return distance;
+}
+
+struct Point
+{
+    public double x;
+    public double y;
+
+    public Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+};
