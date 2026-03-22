5abca815b9c3acdad149b34bbc5555eb4e18eb52
odnoburtsev_vadim <taskevichoff@yandex.ru>
2026-03-22 15:53:12 +0300

    pluralize task

---
+++ AccumulationsOfRubles/PluralizeTask.cs
@@ -0,0 +1,12 @@
+﻿namespace Pluralize;
+
+public static class PluralizeTask
+{
+	public static string PluralizeRubles(int count)
+	{
+		return (count % 100 >= 11 && count % 100 <= 14) ? "рублей" :
+			(count % 10 == 1) ? "рубль" :
+			(count % 10 >= 2 && count % 10 <= 4) ? "рубля" :
+			"рублей";
+	}
+}
